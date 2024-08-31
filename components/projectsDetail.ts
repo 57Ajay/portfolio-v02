71ba146432b91fe53bfb353c7d981e0d67053831
57ajay.u <57ajay.u@gmail.com>
2024-08-31 16:03:26 +0000

    minor fix

--- components/projectsDetail.ts
+++ components/projectsDetail.ts
@@ -2,7 +2,7 @@ export const projectDetails = [
     {
         title: "HomeFindr",
         description: "HomeFindr is an Estate application where users canlist their villas and houses and also see and book listings of others.",
-        imgUrl : "/HomeFindr.jfif",
+        imgUrl : "/images/HomeFindr.jfif",
         techUsed: " React, Node.js, MongoDB, Typescript, Redux, Express, Tailwind CSS, Framer Motion.",
         gitLink: "https://github.com/57Ajay/homefindr",
         liveLink: "https://homefindr-backend.onrender.com",
@@ -10,7 +10,7 @@ export const projectDetails = [
     {
         title: "CashUp",
         description: "Cash is a payment Application where users can create ttheir account and link their bank accounts and transfer payments with others via their username or email.",
-        imgUrl : "/CashUp.jfif",
+        imgUrl : "/images/CashUp.jfif",
         techUsed: " React, MongoDB, Typescript, Redux, Express, Tailwind CSS, Framer Motion.",
         gitLink: "https://github.com/57Ajay/cashup",
         liveLink: "https://github.com/57Ajay/cashup"
@@ -19,7 +19,7 @@ export const projectDetails = [
     {
         title: "NextWrite",
         description: "NextWrite is a blogging site where users can come and write about anything and share it with others",
-        imgUrl : "/NextWrite.jfif",
+        imgUrl : "/images/NextWrite.jfif",
         techUsed: " React, NextJS, MongoDB, Typescript, Tailwind CSS.",
         gitLink: "https://github.com/57Ajay/code-grind",
         liveLink: "https://next-blog-theta-nine-93.vercel.app",
@@ -27,7 +27,7 @@ export const projectDetails = [
     {
         title: "NextCast",
         description: "NextCast is an AI powered podcast application, where users can harness the power of AI you embrace their creativity.",
-        imgUrl : "/NextCast.jfif",
+        imgUrl : "/images/NextCast.jfif",
         techUsed: " React, NextJS, MongoDB, Typescript, Tailwind CSS, OpenAI, Convex.",
         gitLink: "https://github.com/57Ajay/next-cast",
         liveLink: "https://github.com/57Ajay/next-cast",
