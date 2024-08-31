023611c4e42eb74873039d17d8b77b87532904f5
57ajay.u <57ajay.u@gmail.com>
2024-08-31 15:02:41 +0000

    almost done

--- components/projectsDetail.ts
+++ components/projectsDetail.ts
@@ -2,7 +2,7 @@ export const projectDetails = [
     {
         title: "HomeFindr",
         description: "HomeFindr is an Estate application where users canlist their villas and houses and also see and book listings of others.",
-        imgUrl : "/homefindr.png",
+        imgUrl : "/HomeFindr.jfif",
         techUsed: " React, Node.js, MongoDB, Typescript, Redux, Express, Tailwind CSS, Framer Motion.",
         gitLink: "https://github.com/57Ajay/homefindr",
         liveLink: "https://homefindr-backend.onrender.com",
@@ -10,7 +10,7 @@ export const projectDetails = [
     {
         title: "CashUp",
         description: "Cash is a payment Application where users can create ttheir account and link their bank accounts and transfer payments with others via their username or email.",
-        imgUrl : "/next.svg",
+        imgUrl : "/CashUp.jfif",
         techUsed: " React, MongoDB, Typescript, Redux, Express, Tailwind CSS, Framer Motion.",
         gitLink: "https://github.com/57Ajay/cashup",
         liveLink: "https://github.com/57Ajay/cashup"
@@ -19,10 +19,17 @@ export const projectDetails = [
     {
         title: "NextWrite",
         description: "NextWrite is a blogging site where users can come and write about anything and share it with others",
-        imgUrl : "/nextwrite.png",
+        imgUrl : "/NextWrite.jfif",
         techUsed: " React, NextJS, MongoDB, Typescript, Tailwind CSS.",
         gitLink: "https://github.com/57Ajay/code-grind",
         liveLink: "https://next-blog-theta-nine-93.vercel.app",
     },
-
+    {
+        title: "NextCast",
+        description: "NextCast is an AI powered podcast application, where users can harness the power of AI you embrace their creativity.",
+        imgUrl : "/NextCast.jfif",
+        techUsed: " React, NextJS, MongoDB, Typescript, Tailwind CSS, OpenAI, Convex.",
+        gitLink: "https://github.com/57Ajay/next-cast",
+        liveLink: "https://github.com/57Ajay/next-cast",
+    },
 ]
